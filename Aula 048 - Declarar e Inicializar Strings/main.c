f1cbb420fa0791f46d0f74c48821cea5aafd9180
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2021-08-10 19:31:03 -0300

    update 10/08/2021

---
+++ Aula 048 - Declarar e Inicializar Strings/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    // char palavra[6] = {'B,' 'r', 'a', 's', 'i', 'l'};
+    // char palavra[] = {'B,' 'r', 'a', 's', 'i', 'l'};
+    // char palavra[7] = {"Brasil"};
+    // char palavra[] = {"Brasil"};
+    char palavra[] = "Brasil";
+
+    printf("%s\n", palavra);
+    
+    return 0;
+}
